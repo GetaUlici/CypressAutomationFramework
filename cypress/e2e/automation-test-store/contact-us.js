29fcee3024f869f0b2b42f4d8c578cafba3e2880
geta.ulici <ulicigeta@gmail.com>
2025-06-25 16:52:49 +0300

    First tests

---
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+
+
+
+describe("Test Contact Us from via Automation Test Store", () => {
+    it("Should be able to submit a successful submission via Contact Us form", () => {
+      cy.visit("https://www.automationteststore.com/") 
+    //   cy.get('.info_links_footer > :nth-child(5) > a').click()
+        cy.xpath("//a[contains(@href, 'contact')]").click()
+      cy.origin('https://automationteststore.com', () => {
+            cy.get('#ContactUsFrm_first_name').type("Joe");
+            cy.get('#ContactUsFrm_email').type("joe_blogs@example.com");
+            cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email')
+            cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?");
+            cy.get('button[title="Submit"]').click();
+            cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!')
+        });
+    
+
+    })
+
+})
---
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+
+describe("Test Contact Us from via WebdriverUni", () => {
+    it("Should be able to submit a successful submission via Contact Us form", () => {
+        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.document().should('have.property', 'charset').and('eq', ('UTF-8'))
+        cy.title().should('include', 'WebDriver | Contact Us')
+        cy.url().should('include', 'contactus')
+        cy.get('[name="first_name"]').type("Joe")
+        cy.get('[name="last_name"]').type("Doe")
+        cy.get('[name="email"]').type('email@email.com')
+        cy.get('textarea.feedback-input').type('This is a funny message')
+        cy.get('[type="submit"]').click()
+        cy.get('h1').should('have.text', 'Thank You for your Message!')
+    })
+
+    it("Should not be able to submit a successful submission via Contact Us form as all fields are required", () => {
+        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.get('[name="first_name"]').type("Joe")
+        cy.get('[name="last_name"]').type("Doe")
+        cy.get('textarea.feedback-input').type('This is a funny message')
+        cy.get('[type="submit"]').click()
+        cy.get('body').contains('Error: all fields are required')
+    })
+})
